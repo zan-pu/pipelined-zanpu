1f7694af9920dc133efe3201ebd91f416ec335f8
spencerwoo <spencerwoo98@foxmail.com>
2019-09-07 17:04:55 +0800

    update: start testing

--- pipelined-zanpu.srcs/sim_1/new/testbench.v
+++ pipelined-zanpu.srcs/sim_1/new/testbench.v
@@ -1,26 +1,33 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/09/05 20:29:14
-// Design Name: 
-// Module Name: testbench
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+/*
+ * Testbench
+ */
 
-module testbench(
+module testbench();
+reg clk;
+reg rst;
+zanpu_top u_zanpu_top(
+              .clk (clk ),
+              .rst (rst )
+          );
 
-    );
+initial begin
+    // Load instructions
+    $readmemh("../../../pipelined-zanpu.tbcode/instructions.txt", u_zanpu_top.u_instruction_memory.im);
+    // Load register initial values
+    $readmemh("../../../pipelined-zanpu.tbcode/register.txt", u_zanpu_top.u_register_file.gpr);
+    // Load memory data initial values
+    $readmemh("../../../pipelined-zanpu.tbcode/data_memory.txt", u_zanpu_top.u_data_memory.dm);
+
+    rst = 1;
+    clk = 0;
+
+    #30 rst = 0;
+    // #80 $display("$10 value: %h", ZAN_TOP.ZAN_REG_FILE.gpr[10]);
+    #500 $stop;
+end
+
+always
+    #20 clk = ~clk;
 endmodule
